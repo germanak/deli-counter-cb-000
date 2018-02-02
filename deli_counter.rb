884b4361e2cce3adbdae67610956d6f744d49fdf
Alec German <alec_german@yahoo.com>
2018-02-02 03:31:24 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,4 @@
 # Write your code here.
 def line(queue)
-  if queue.length == 0
-    puts "The line is currently empty."
-  else
-    puts "The line is currently: " + queue.map.with_index{|customer, index| "#{index + 1}. #{customer}"}.join(' ')
-  end
+  queue.length == 0 ? puts "The line is currently empty." : puts "The line is currently: " + queue.map.with_index{|customer, index| "#{index + 1}. #{customer}"}.join(' ')
 end
