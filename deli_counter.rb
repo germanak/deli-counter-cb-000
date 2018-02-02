17affc4082b3f1819a0e34a1fbb861cc5dca0edf
Alec German <alec_german@yahoo.com>
2018-02-02 03:31:53 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,6 +1,6 @@
 # Write your code here.
 def line(queue)
-  if queue.length == 0 
+  if queue.length == 0
     puts "The line is currently empty."
   else
     puts "The line is currently: " + queue.map.with_index{|customer, index| "#{index + 1}. #{customer}"}.join(' ')
