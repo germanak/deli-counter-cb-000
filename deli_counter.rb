3267156c14713262a1c832590a9ef51dc33c0759
Alec German <alec_german@yahoo.com>
2018-02-02 03:20:42 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,6 +3,6 @@ def line(queue)
   if queue.length == 0
     puts "The line is currently empty."
   else
-    puts "The line is currently: " + array.map_with_index {|customer, index| "#{index + 1}. #{customer}"}
+    puts "The line is currently: " + array.map_with_index {|customer, index| "#{index + 1}. #{customer}"}.join(' ')
   end
 end
